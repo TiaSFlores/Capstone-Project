2ef98f21994573ff6ba4eb6d5117845e606057ad
Youjin814 <72735181+Youjin814@users.noreply.github.com>
2023-03-01 20:12:23 -0800

    Update index.tsx

--- travel-planner/src/index.tsx
+++ travel-planner/src/index.tsx
@@ -6,8 +6,13 @@ import App from './App';
 import Login from './Login';
 import Register from './Register';
 import LandingPage from './LandingPage';
+import Experience from './Experience';
 
 const router = createBrowserRouter([
+  {
+    path: "/experience",
+    element: <Experience/>
+  },
   {
     path: "/login",
     element: <Login/>
